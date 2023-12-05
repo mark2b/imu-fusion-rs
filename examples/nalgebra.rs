529c077f2dfd9f70a4896d05cc7bcc952c635277
Mark Berner <mark@berner.dev>
2023-12-05 22:40:53 +0200

    nalgebra example added

---
+++ examples/nalgebra.rs
@@ -0,0 +1,16 @@
+use imu_fusion::FusionAhrsSettings;
+use nalgebra::{Vector3};
+
+const SAMPLE_RATE_HZ: u32 = 100;
+
+fn main() {
+    let ahrs_settings = FusionAhrsSettings::new();
+    let mut fusion = imu_fusion::Fusion::new(SAMPLE_RATE_HZ, ahrs_settings);
+    loop {
+        let gyr = Vector3::new(0f32, 0f32, 0f32); // replace this with actual gyroscope data in degrees/s
+        let acc = Vector3::new(0f32, 0f32, 1.0f32); // replace this with actual accelerometer data in g
+        fusion.update_no_mag(gyr.into(), acc.into(), 1.0 / SAMPLE_RATE_HZ as f32);
+        let euler = fusion.euler();
+        println!("Roll {}, Pitch {}, Yaw {}", euler.angle.roll, euler.angle.pitch, euler.angle.yaw);
+    }
+}
